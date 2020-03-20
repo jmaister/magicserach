7d0bfe50f6d1376079d28144273be88c2a213b3a
Jordi Burgos <jordiburgos@gmail.com>
2020-03-20 00:38:58 +0000

    Setup http server with gunicorn

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 git pull
-pkill -SIGTERM flask
+pkill -SIGTERM gunicorn
 source magicsearch/bin/activate
-export FLASK_APP=app
-export FLASK_ENV=production
-nohup flask run -h 127.0.0.1 -p 5000 --with-threads --reload >> ms.log &
+#export FLASK_APP=app
+#export FLASK_ENV=production
+nohup gunicorn -b "127.0.0.1:5000" --timeout 60 --workers 2  app:app >> ms.log &
 
 sleep 5
 curl 127.0.0.1:5000/runrules
